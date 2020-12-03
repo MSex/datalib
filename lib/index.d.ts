9ea1a5b4f0c44ec8b0874d3a9d3eff7796d8ff7e
Mauricio Moura <mauricio@topster.com.br>
2020-12-03 18:23:14 -0300

    dois

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,22 @@
-export declare const Greeter: (name: string) => string;
+import "reflect-metadata";
+import { BehaviorSubject, Observable } from "rxjs";
+export declare const TYPES: {
+    Pessoas: symbol;
+};
+export interface Pessoas {
+    list(): Observable<Pessoa[]>;
+    add(): void;
+    refresh(): void;
+}
+export declare class Pessoa {
+    id: number;
+    name: string;
+    constructor(id: number, name: string);
+}
+export declare class PessoasImpl implements Pessoas {
+    private _storage;
+    private _list;
+    list(): BehaviorSubject<Pessoa[]>;
+    refresh(): void;
+    add(): void;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import { injectable } from "inversify";
+import "reflect-metadata";
+import { BehaviorSubject, Observable } from "rxjs";
+
+export const TYPES = {
+  Pessoas: Symbol("Pessoas")
+};
+
+export interface Pessoas {
+  list(): Observable<Pessoa[]>;
+  add(): void;
+  refresh(): void;
+}
+
+export class Pessoa {
+  public id: number;
+  public name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+@injectable()
+export class PessoasImpl implements Pessoas {
+  private _storage = [
+    new Pessoa(1, "MSex"),
+    new Pessoa(2, "Guto"),
+    new Pessoa(3, "Renato")
+  ];
+  private _list = new BehaviorSubject<Pessoa[]>(this._storage);
+
+  public list() {
+    return this._list;
+  }
+
+  public refresh() {
+    this._list.next([]);
+    this._list.next(this._storage);
+  }
+
+  public add() {
+    const n = this._storage.length + 1;
+    this._storage.push(new Pessoa(n, "User" + n));
+  }
+}
